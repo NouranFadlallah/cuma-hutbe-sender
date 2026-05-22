fc4c740a2f6716d944bf96ed31113a8266792362
Nouran Fadlallah <nouran.fadlallah@gmail.com>
2026-05-22 17:03:24 +0300

    changed header

--- cuma_hutbe.py
+++ cuma_hutbe.py
@@ -32,7 +32,10 @@ image_filename = "converted_page"
 
 def fetch_pdf_and_save_image(url: str, num: int):
     # Fetch the page
-    response = requests.get(url)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }     # Add standard browser headers
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'html.parser')
 
